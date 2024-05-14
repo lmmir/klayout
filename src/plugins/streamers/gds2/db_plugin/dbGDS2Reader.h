fdc32e02c42bb1ac8702a1bf372738cf39e0fbad
yangqi <power5455@163.com>
2024-05-14 16:57:17 +0800

    添加注释

--- src/plugins/streamers/gds2/db_plugin/dbGDS2Reader.h
+++ src/plugins/streamers/gds2/db_plugin/dbGDS2Reader.h
@@ -20,97 +20,92 @@
 
 */
 
-
-
 #ifndef HDR_dbGDS2Reader
 #define HDR_dbGDS2Reader
 
-#include "dbPluginCommon.h"
-#include "dbLayout.h"
+#include "dbCommonReader.h"
 #include "dbGDS2Format.h"
 #include "dbGDS2ReaderBase.h"
-#include "dbCommonReader.h"
+#include "dbLayout.h"
+#include "dbPluginCommon.h"
 #include "dbStreamLayers.h"
 
 #include "tlException.h"
 #include "tlInternational.h"
 #include "tlProgress.h"
-#include "tlString.h"
 #include "tlStream.h"
+#include "tlString.h"
 
-namespace db
-{
+namespace db {
 
 /**
  *  @brief Generic base class of GDS2 reader exceptions
  */
-class DB_PLUGIN_PUBLIC GDS2ReaderException
-  : public ReaderException 
-{
+class DB_PLUGIN_PUBLIC GDS2ReaderException : public ReaderException {
 public:
-  GDS2ReaderException (const std::string &msg, size_t p, size_t n, const std::string &cell)
-    : ReaderException (tl::sprintf (tl::to_string (tr ("%s (position=%ld, record number=%ld, cell=%s)")), msg, p, n, cell))
-  { }
+  GDS2ReaderException(const std::string &msg, size_t p, size_t n,
+                      const std::string &cell)
+      : ReaderException(tl::sprintf(
+            tl::to_string(tr("%s (position=%ld, record number=%ld, cell=%s)")),
+            msg, p, n, cell)) {}
 };
 
 /**
  *  @brief The GDS2 format stream reader
  */
-class DB_PLUGIN_PUBLIC GDS2Reader
-  : public GDS2ReaderBase
-{
-public: 
+class DB_PLUGIN_PUBLIC GDS2Reader : public GDS2ReaderBase {
+public:
   /**
    *  @brief Construct a stream reader object
    *
    *  @param s The stream delegate from which to read stream data from
    */
-  GDS2Reader (tl::InputStream &s);
+  GDS2Reader(tl::InputStream &s);
 
-  /**  
+  /**
    *  @brief Destructor
    */
-  ~GDS2Reader ();
+  ~GDS2Reader();
 
   /**
    *  @brief Format
    */
-  virtual const char *format () const { return "GDS2"; }
+  virtual const char *format() const { return "GDS2"; }
 
 protected:
-  virtual void init (const LoadLayoutOptions &options);
+  virtual void init(const LoadLayoutOptions &options);
 
 private:
   tl::InputStream &m_stream;
-  size_t m_recnum;
-  size_t m_reclen;
-  size_t m_recptr;
-  unsigned char *mp_rec_buf;
-  tl::string m_string_buf;
+  size_t m_recnum;           //记录record的数量
+  size_t m_reclen;           // record长度
+  size_t m_recptr;           // 相对于mp_rec_buf的指针。
+  unsigned char *mp_rec_buf; // record 数据的起始指针
+  tl::string m_string_buf;   //字符串缓冲区
+
   short m_stored_rec;
   bool m_allow_big_records;
   tl::AbsoluteProgress m_progress;
 
-  virtual void error (const std::string &txt);
-  virtual void warn (const std::string &txt, int wl = 1);
+  virtual void error(const std::string &txt);
+  virtual void warn(const std::string &txt, int wl = 1);
 
-  virtual std::string path () const;
-  virtual const char *get_string ();
-  virtual void get_string (std::string &s) const;
-  virtual int get_int ();
-  virtual short get_short ();
-  virtual unsigned short get_ushort ();
-  virtual double get_double ();
-  virtual short get_record ();
-  virtual void unget_record (short rec_id);
-  virtual void get_time (unsigned int *mod_time, unsigned int *access_time);
-  virtual GDS2XY *get_xy_data (unsigned int &length);
-  virtual void progress_checkpoint ();
+  virtual std::string path() const;
+  virtual const char *get_string();
+  virtual void get_string(std::string &s) const;
+  virtual int get_int();
+  virtual short get_short();
+  virtual unsigned short get_ushort();
+  virtual double get_double();
+  virtual short get_record();
+  virtual void unget_record(short rec_id);
+  virtual void get_time(unsigned int *mod_time, unsigned int *access_time);
+  virtual GDS2XY *get_xy_data(unsigned int &length);
+  virtual void progress_checkpoint();
 
-  void record_underflow_error ();
+  void record_underflow_error();
 };
 
-}
+} // namespace db
 
 #endif
-
